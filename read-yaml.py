41082d62de073bd1cbf7948bd8f4f0645bf5dea7
dps3ven <dpitpro@gmail.com>
2025-08-17 03:31:37 -0500

    python download s3

--- read-yaml.py
+++ read-yaml.py
@@ -1,31 +1,37 @@
 import yaml
 import requests
+import boto3
 
-def get_all_tenants():
-    with open('renters.yml', 'r') as file:
-        data = yaml.safe_load(file)
-    return data['tenants']
+def download_tenant_yaml():
+    s3 = boto3.client('s3')
+    s3.download_file("vindot-llc-tenants", "tenants.yml", "tenants.yml")
 
-def get_tenant_names():
-    with open('renters.yml', 'r') as file:
-        data = yaml.safe_load(file)
-    return [tenant['name'] for tenant in data['tenants']]
+# def get_all_tenants():
+#     with open('renters.yml', 'r') as file:
+#         data = yaml.safe_load(file)
+#     return data['tenants']
 
-def get_tenant_unit():
-    with open('renters.yml', 'r') as file:
-        data = yaml.safe_load(file)
-    return [tenant['unit'] for tenant in data['tenants']]
+# def get_tenant_names():
+#     with open('renters.yml', 'r') as file:
+#         data = yaml.safe_load(file)
+#     return [tenant['name'] for tenant in data['tenants']]
+
+# def get_tenant_unit():
+#     with open('renters.yml', 'r') as file:
+#         data = yaml.safe_load(file)
+#     return [tenant['unit'] for tenant in data['tenants']]
 
 def verify_tenant(name, unit, phone):
-    with open('renters.yml', 'r') as file:
+    status = False
+    with open('tenants.yml', 'r') as file:
         data = yaml.safe_load(file)
     
     for tenant in data['tenants']:
         if (tenant['name'].lower() == name.lower() and 
             tenant['unit'] == unit and 
             tenant['phone'] == phone):
-            return True
-    return False
+            status = True
+    return status
 
 # def call_amazon_api(name, unit):
 #     if verify_tenant(name, unit):
@@ -35,8 +41,15 @@ def verify_tenant(name, unit, phone):
 #     return None
 
 if __name__ == "__main__":
+    download_tenant_yaml()
     # print(get_all_tenants())
     # print(get_tenant_names())
     # print(get_tenant_unit())
-    print(verify_tenant("Paco Greenwell", "5069B", "555-0456"))
+    # print(verify_tenant("Paco Greenwell", "5069B", "555-0456"))
+    # print(verify_tenant("Pinko Greenwell", "5069B", "555-0456"))
+    result = (verify_tenant("Paci Greenwell", "5069B", "555-0456"))
+    if result == True:
+        print("Forward to LLC")
+    else:
+        print("Rejected")
     # print(call_amazon_api("XXX", "XXXXX"))
